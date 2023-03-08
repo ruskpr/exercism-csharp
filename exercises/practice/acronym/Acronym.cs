6f43d849cbd2c293b17a548f0cd765bfb2046b38
Rustu Koprulu <rustulp@gmail.com>
2023-03-07 22:26:59 -0700

    updated

--- exercises/practice/acronym/Acronym.cs
+++ exercises/practice/acronym/Acronym.cs
@@ -4,6 +4,21 @@ public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        throw new NotImplementedException("Please implement this function");
+        string ret = string.Empty;
+        phrase.Replace('-', ' ').Replace('_', ' ');
+        string[] words = phrase.Split(" ");
+
+
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = 0; j < words[i].Length; j++)
+            {
+
+            }
+            ret += words[i].ToCharArray()[0];
+        }
+
+        return ret.ToUpper();
     }
 }
