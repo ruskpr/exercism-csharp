f40907147741f404977ac6e84f191c40121ead40
Russ Koprulu <rustulp@gmail.com>
2023-03-07 23:01:46 -0700

    updated

--- exercises/practice/acronym/Acronym.cs
+++ exercises/practice/acronym/Acronym.cs
@@ -5,15 +5,30 @@ public static class Acronym
     public static string Abbreviate(string phrase)
     {
         string ret = "";
-
+        //phrase.Replace("-", " - ");
         string[] words = phrase.Split(' ');
 
-        for (int i = 0; i < words.Length; i++)// loop through each word
+        for (int i = 0; i < words.Length; i++) // loop through each word
         {
-            for (int j = 0; j < words[i].Length; j++) // loop through each word's letter
+            // hyphenated word check
+            if (words[i].Contains("-") && words[i].Length > 1)
             {
+                string[] hyphenatedWord = words[i].Split('-');
+                for (int j = 0; j < hyphenatedWord.Length; j++)
+                {
+                    ret += hyphenatedWord[j].ToCharArray()[0];
+                }
 
+                continue;
             }
+
+            if (words[i] == "-")
+                continue;
+
+            // underscore check
+            words[i] = words[i].Trim('_');
+
+            ret += words[i].ToCharArray()[0];
         }
 
         return ret.ToUpper();
--- exercises/practice/affine-cipher/AffineCipher.cs
+++ exercises/practice/affine-cipher/AffineCipher.cs
@@ -1,14 +1,43 @@
 using System;
-
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
 public static class AffineCipher
 {
+    private const int Alphabet = 26;
+    private const int ChunkSize = 5;
     public static string Encode(string plainText, int a, int b)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!IsCoprime(a, Alphabet))
+            throw new ArgumentException("Numbers must be coprime");
+        var encoded = plainText.ToLower().Where(char.IsLetterOrDigit)
+                               .Select(x => char.IsDigit(x) ? x : ToChar((a * Index(x) + b) % Alphabet));
+        return string.Join(" ", encoded.Chunk(ChunkSize).Select(x => new string(x.ToArray())));
     }
-
+    private static IEnumerable<IEnumerable<char>> Chunk(this IEnumerable<char> source, int size)
+    {
+        while (source.Any())
+        {
+            yield return source.Take(size);
+            source = source.Skip(size);
+        }
+    }
     public static string Decode(string cipheredText, int a, int b)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!IsCoprime(a, Alphabet))
+            throw new ArgumentException("Numbers must be coprime");
+        return new string(cipheredText.Where(char.IsLetterOrDigit)
+                                      .Select(x => char.IsDigit(x) ? x : DecodeSymbol(x, a, b))
+                                      .ToArray());
     }
+    private static char DecodeSymbol(char c, int a, int b)
+    {
+        var value = Mmi(a) * (Index(c) - b) % Alphabet;
+        if (value < 0) value += Alphabet;
+        return ToChar(value);
+    }
+    private static int Mmi(int a) => Enumerable.Range(1, Alphabet).First(x => (a * x % Alphabet) == 1);
+    private static int Index(char c) => c - 'a';
+    private static char ToChar(int i) => (char)('a' + i);
+    private static bool IsCoprime(int a, int m) => a == 0 ? m == 1 : IsCoprime(m % a, a);
 }
