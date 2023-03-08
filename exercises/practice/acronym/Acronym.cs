7b1c9d0f0f245325886bf6eb0b68aeb0ef08ea8a
Russ Koprulu <rustulp@gmail.com>
2023-03-07 22:31:49 -0700

    updated

--- exercises/practice/acronym/Acronym.cs
+++ exercises/practice/acronym/Acronym.cs
@@ -4,6 +4,18 @@ public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        throw new NotImplementedException("Please implement this function");
+        string ret = "";
+
+        string[] words = phrase.Split(' ');
+
+        for (int i = 0; i < words.Length; i++)// loop through each word
+        {
+            for (int j = 0; j < words[i].Length; j++) // loop through each word's letter
+            {
+
+            }
+        }
+
+        return ret.ToUpper();
     }
 }
